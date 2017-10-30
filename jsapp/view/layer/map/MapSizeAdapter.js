96866d0760887d692048097923d7d0148cc85704
atc48 <atc48er@gmail.com>
2017-10-30 19:00:56 +0900

    fix; moved scale, move handler into MapSizeAdapter.

--- jsapp/view/layer/map/MapSizeAdapter.js
+++ jsapp/view/layer/map/MapSizeAdapter.js
@@ -16,23 +16,26 @@
     this.uiCommand = uiCommand;
     this.stageSizeMan = stageSizeMan;
 
-    this.uiCommand = uiCommand.on("zoom", _.bind(this._onZoomInput, this));
+    this.uiCommand.on("zoom", _.bind(this._onZoomInput, this));
     this.curScale = DEFAULT_SCALE;
     this.container.scaleX = this.container.scaleY = this.curScale;
+
+    this.uiCommand.on("mapMove", _.bind(this._onMapMove, this));
   }
 
   MapSizeAdapter.prototype._onZoomInput = function(e) {
     var nextScale = this.container.scaleX * (1.0 + ZOOM_MULT * (e.delta > 0 ? 1 : -1))
-    this.updateScale(nextScale);
-    this.curScale = nextScale;
-    this.fire({scale: nextScale});
+    this.updateScaleWithCenterFix(nextScale);
   };
 
-  MapSizeAdapter.prototype.updateScale = function (scale) {
+  MapSizeAdapter.prototype.updateScaleWithCenterFix = function (scale) {
     __.assert(_.isNumber(scale));
     var oldCenterLocal = __getStageCenterLocalPos(this.container, this.stageSizeMan);
     this.container.scaleX = this.container.scaleY = scale;
     __setStageCenterLocalPos(this.container, this.stageSizeMan, oldCenterLocal);
+
+    this.curScale = nextScale;
+    this.fire("scale", {scale: nextScale});
   };
 
   function __getStageCenterLocalPos(container, stageSizeMan) {
@@ -50,6 +53,12 @@
     container.x -= diff.x;
     container.y -= diff.y;
   }
+
+  MapSizeAdapter.prototype._onMapMove = function (e) {
+    this.container.x += e.moveX;
+    this.container.y += e.moveY;
+    this.fire("move", {});
+  };
 
   return MapSizeAdapter;
 });
