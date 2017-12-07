42965209381c7f87fe52bfb89cee6a52d3633fc8
atc48 <atc48er@gmail.com>
2017-12-07 18:29:42 +0900

    fix; implemented RadioAidParser.

--- pytool/aip/fix_finder.py
+++ pytool/aip/fix_finder.py
@@ -17,6 +17,7 @@ FIX_CODE_SUBS = [
 
 RADIO_AID_FILE = "./data/JP-ENR-4.1-en-JP.html"
 RADIO_AID_SUBS = [
+    [r"ZAO\sYAMADA", "ZAOYAMADA"]
 ]
 
 
@@ -24,9 +25,8 @@ class Fix:
     FIX     = 'FIX'
     VORDME  = 'VORDME'
     VORTAC  = 'VORTAC'
-    VOR     = 'VOR' # nop
-    DME     = 'DME' # nop
-    CATEGORIES = [FIX, VORDME, VORTAC, VOR, DME]
+    NDB     = 'NDB'
+    CATEGORIES = [FIX, VORDME, VORTAC, NDB]
 
     def __init__(self, code, category, pron=None):
         self.code = code.strip()
@@ -35,6 +35,8 @@ class Fix:
         self.coordinate = None
         assert self.CATEGORIES.index(category) >= 0, self
         if category == Fix.FIX: assert len(code) == 5, self
+        elif category == Fix.NDB: assert len(code) == 2, self
+        else: assert len(code) == 3, self
         if pron: assert category != Fix.FIX, self
 
     def __str__(self):
@@ -64,8 +66,8 @@ class Fix:
 class AbstractAipTableParser:
 
     def __init__(self, filepath, div_id):
-        self.fixes = []
-        self.unknown_codes = []
+        self._fixes = []     # accessible from child class
+        self._unknowns = []  # accessible from child class
         self.__filepath = filepath
         self.__div_id   = div_id
         
@@ -80,12 +82,118 @@ class AbstractAipTableParser:
 
         trs = tbody.find_all('tr')
         for tr in trs:
-            self.parse_tr(tr)
+            self._parse_tr(tr)
 
-        for f in self.fixes: _print(f)
-        for c in self.unknown_codes: _print(c)
+        self._compact_fixes()
+
+        for f in self._fixes: _print(f)
+        _print("---")
+        for c in self._unknowns: _print(c)
+        _print("---")
+
+        self.identify_info = {
+            'num_fixes': len(self._fixes),
+            'num_unknown': len(self._unknowns)
+        }
+        for fix in self._fixes:
+            assert 1 == len([f for f in  self._fixes if fix == f])
+
+        self._after_parse()
 
         return self
+
+    def _parse_tr(self, tr):
+        assert False, "not implemented _parse_tr on child class."
+    
+    def _compact_fixes(self):
+        None
+
+    def _after_parse(self):
+        None
+
+class RadioAidParser(AbstractAipTableParser):
+    FILE = RADIO_AID_FILE
+    POSTFIX_CATE_MAP = [
+        [r"VOR/DME$"  , Fix.VORDME],
+        [r"(VOR|DME)$", Fix.VORDME],
+        [r"TACAN$"    , Fix.VORTAC],
+        [r"NDB$"      , Fix.NDB]
+    ]
+    REMOVE_EXP = r"\((decl|var)\.\:[^)]+\)"
+
+    def __init__(self):
+        super().__init__(
+            self.FILE,
+            'ENR-4.1'
+        )
+        self.__dup_removed = []
+
+    def _after_parse(self):
+        for dup in self.__dup_removed:
+            selected = [f for f in self._fixes if f.code == dup.code][0]
+            _print(str(selected) + " <= " + str(dup) )
+
+        self.identify_info['num_dup'] = len(self.__dup_removed)
+        
+
+    def _parse_tr(self, tr):
+        tds = tr.find_all('td')
+        assert len(tds) >= 2, "Unknown tds style." + str(tds)
+
+        name_s, code, cdn_s =  [str(tds[i].get_text()).strip()
+                              for i in [0, 1, 4]]
+
+        name, category = self.__parse_name_text(name_s)
+        if not name or not category:
+            self._unknowns.append( name_s )
+            return
+
+        assert re.match(r"^[A-Z]+$", name), name
+        assert re.match(r"^[A-Z]{2,3}$", code), code
+
+        fix = Fix(code, category, name)
+        self._fixes.append( fix )
+
+        return fix
+
+    def __parse_name_text(self, text):
+        text = re.sub(self.REMOVE_EXP, "", text).strip()
+        cate_map = [ m for m in self.POSTFIX_CATE_MAP if re.search(m[0], text) ]
+        if not cate_map:
+            return [None, None]
+        cate_map = cate_map[0]
+
+        name = re.sub(cate_map[0], "", text).strip()
+        cate = cate_map[1]
+
+        for r in RADIO_AID_SUBS:
+            name = re.sub(r[0], r[1], name)
+
+        return [name, cate]
+
+    def _compact_fixes(self):
+        i = 0
+        before_len = len(self._fixes)
+        while len(self._fixes) > i:
+            fix = self._fixes[i]
+            i += 1
+            if len(self._fixes) <= i:
+                continue
+            nextfix = self._fixes[i]
+            if fix.code == nextfix.code:
+                assert fix.pron == fix.pron, "code same but pron not same"
+                if fix.category == Fix.VORDME and nextfix.category == Fix.VORDME:
+                    self.__dup_removed.append( self._fixes.pop(i) )
+                elif fix.category == Fix.VORDME and nextfix.category == Fix.VORTAC:
+                    self.__dup_removed.append( self._fixes.pop(i-1) )
+                else:
+                    _print("---")
+                    _print(fix)
+                    _print(nextfix)
+                    assert False
+                assert len(self._fixes) > i+1 or self._fixes[i+1].code != fix.code
+
+        assert before_len == len(self.__dup_removed) + len(self._fixes)
 
 
 class FixParser(AbstractAipTableParser):
@@ -98,28 +206,29 @@ class FixParser(AbstractAipTableParser):
             'ENR-4.3'
         )
 
-    def parse_tr(self, tr):
+    def _parse_tr(self, tr):
         tds = tr.find_all('td')
         assert len(tds) >= 2, "Unknown tds style." + str(tds)
 
-        code =  str(tds[0].get_text())
-        coordinate_str = str(tds[1].get_text()).strip()
+        code, cdn_s = [ str(tds[i].get_text()).strip()
+                                 for i in [0,1] ]
         
         code = self.__filter_code(code)
         if not code.strip():
+            self._unknowns.append( tds )
             _print("none-line:" + str(tds))
             return None
         
         assert re.match(r"^[A-Z]+$", code), code
         if len(code) != 5:
-            self.unknown_codes.append( code )
+            self._unknowns.append( code )
             # TODO: fetch navaids instead insert fix,
             return None
 
-        coordinate = Coordinate.parse_coordinate(coordinate_str)
+        coordinate = Coordinate.parse_coordinate(cdn_s)
 
         fix = Fix(code, Fix.FIX).set_coordinate(coordinate)
-        self.fixes.append( fix )
+        self._fixes.append( fix )
 
         return fix
 
@@ -129,9 +238,22 @@ class FixParser(AbstractAipTableParser):
         return code
 
 
+
+#TODO: make navaids finder and set it into fix finder
+#TODO: fix finder
+
+
+
+
 def main():
     fix_parser = FixParser()
     fix_parser.parse()
+    assert fix_parser.identify_info == {'num_fixes': 1972, 'num_unknown': 57}
+
+
+    rdo_parser = RadioAidParser()
+    rdo_parser.parse()
+    assert rdo_parser.identify_info == {'num_fixes': 129, 'num_unknown': 0, 'num_dup': 21}
 
 
 def _print(str):
